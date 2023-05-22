fa83a9ea2776c440ab56c3a7755ada6de83d3085
Oumaïma <120368654+oumaimabenaboud@users.noreply.github.com>
2023-05-22 22:26:02 +0100

    second

--- customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package org.sid.customerservice;
 
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +13,15 @@ public class CustomerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.save(new Customer(null,"Oumaïma","benouma85@gmail.com"));
+			customerRepository.save(new Customer(null,"Khalid","khalid@gmail.com"));
+			customerRepository.save(new Customer(null,"Hassan","hassan@gmail.com"));
+			customerRepository.findAll().forEach((c->{
+				System.out.println((c.toString()));
+			}));
+		};
+	}
 }
---
+++ customer-service/src/main/java/org/sid/customerservice/entities/Customer.java
@@ -0,0 +1,20 @@
+package org.sid.customerservice.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+}
